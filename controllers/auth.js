4e3fcbe7773c3127b7a4745e5d0b671b09757b9f
alex <camward@yandex.ru>
2018-11-17 11:44:47 +0400

    авторизация. проверка e-mail https://httpstatuses.com/

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,12 +11,16 @@ module.exports.login = function(req, res) {
 }
 
 
-module.exports.register = function(req, res) {
+module.exports.register = async function(req, res) {
   // email password
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  })
+  const candidate = await User.findOne({email: req.body.email})
 
-  user.save().then(() => console.log('User created'))
+  if (candidate) {
+    // Пользователь существует, нужно отправить ошибку
+    res.status(409).json({
+      message: 'Такой email уже занят. Попробуйте другой.'
+    })
+  } else {
+    // Нужно создать пользователя
+  }
 }
