3ee37929206caf12872633a167ae8a3ab9a8b7c6
alex <camward@yandex.ru>
2018-11-17 13:11:35 +0400

    авторизация. обработка ошибок

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 const keys = require('../config/keys')
+const errorHandler = require('../utils/errorHandler')
 
 
 module.exports.login = async function(req, res) {
@@ -57,7 +58,7 @@ module.exports.register = async function(req, res) {
       await user.save()
       res.status(201).json(user)
     } catch(e) {
-      // Обработать ошибку
+      errorHandler(res, e)
     }
 
   }
