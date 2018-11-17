ba8402920730d3a9e6331c2b64a062b753d3299b
alex <camward@yandex.ru>
2018-11-17 12:29:53 +0400

    авторизация. вход в систему npm install jsonwebtoken https://jwt.io/

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://camward:camward123@ds117960.mlab.com:17960/fullstack'
+  mongoURI: 'mongodb://camward:camward123@ds117960.mlab.com:17960/fullstack',
+  jwt: 'dev-jwt'
 }
