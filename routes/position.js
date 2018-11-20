b7bb142347b265d2da85a388c26cbbb5cc7fc5ee
alex <camward@yandex.ru>
2018-11-20 20:08:58 +0400

    настройка запуска

--- routes/position.js
+++ routes/position.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const controller = require('../controllers/position')
+const passport = require('passport')
 const router = express.Router()
 
 router.get('/:categoryId', passport.authenticate('jwt', {session: false}), controller.getByCategoryId)
