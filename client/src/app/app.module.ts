6ecbdc94c054e21f5664faed2bd2184385ba5b2b
alex <camward@yandex.ru>
2018-11-24 18:08:12 +0400

    страница заказа

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,10 +14,12 @@ import {OverviewPageComponent} from './overview-page/overview-page.component'
 import {AnalyticsPageComponent} from './analytics-page/analytics-page.component'
 import {HistoryPageComponent} from './history-page/history-page.component'
 import {OrderPageComponent} from './order-page/order-page.component'
-import {CategoriesPageComponent} from './categories-page/categories-page.component';
-import { LoaderComponent } from './shared/components/loader/loader.component';
-import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
-import { PositionsFormComponent } from './categories-page/categories-form/positions-form/positions-form.component'
+import {CategoriesPageComponent} from './categories-page/categories-page.component'
+import {LoaderComponent} from './shared/components/loader/loader.component'
+import {CategoriesFormComponent} from './categories-page/categories-form/categories-form.component'
+import {PositionsFormComponent} from './categories-page/categories-form/positions-form/positions-form.component'
+import {OrderCategoriesComponent} from './order-page/order-categories/order-categories.component'
+import {OrderPositionsComponent} from './order-page/order-positions/order-positions.component'
 
 
 @NgModule({
@@ -35,6 +37,8 @@ import { PositionsFormComponent } from './categories-page/categories-form/positi
     LoaderComponent,
     CategoriesFormComponent,
     PositionsFormComponent,
+    OrderCategoriesComponent,
+    OrderPositionsComponent,
   ],
   imports: [
     BrowserModule,
