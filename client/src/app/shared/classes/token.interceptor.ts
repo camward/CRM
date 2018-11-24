13c25ef29f69559404f0831426456c10c1f22310
alex <camward@yandex.ru>
2018-11-24 16:04:48 +0400

    настройка основной темы

--- client/src/app/shared/classes/token.interceptor.ts
+++ client/src/app/shared/classes/token.interceptor.ts
@@ -1,13 +1,11 @@
 import {Injectable} from '@angular/core'
 import {AuthService} from '../services/auth.service'
-import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http'
-import {Observable, throwError} from 'rxjs'
-import {catchError} from 'rxjs/operators'
-import {Router} from '@angular/router'
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http'
+import {Observable} from 'rxjs'
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService, private router: Router) {
+  constructor(private auth: AuthService) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -18,22 +16,6 @@ export class TokenInterceptor implements HttpInterceptor {
         }
       })
     }
-    return next.handle(req).pipe(
-      catchError(
-        (error: HttpErrorResponse) => this.handleAuthError(error)
-      )
-    )
-  }
-
-  private handleAuthError(error: HttpErrorResponse): Observable<any> {
-    if (error.status === 401) {
-      this.router.navigate(['/login'], {
-        queryParams: {
-          sessionFailed: true
-        }
-      })
-    }
-
-    return throwError(error)
+    return next.handle(req)
   }
 }
